384f648e6df60cac19f1cbdc85f8088fd3786e20
maitreebain <maitreebain@gmail.com>
2019-11-19 16:49:59 -0500

    finished closures in GOT data file

--- GameOfThrones/GameOfThrones/GOTEpisdoe.swift
+++ GameOfThrones/GameOfThrones/GOTEpisdoe.swift
@@ -98,4 +98,28 @@ class GOTEpisode {
         GOTEpisode(airdate: "2017-08-20", id: 1221414, name: "Beyond the Wall", number: 6, season: 7, runtime: 60, summary: "Jon's mission continues north of the wall, but the odds against his ragged band of misfits may be greater than he imagined.", mediumImageID: "312651", originalImageID: "312651"),
         GOTEpisode(airdate: "2017-08-27", id: 1221415, name: "The Dragon and the Wolf", number: 7, season: 7, runtime: 60, summary: "Cersei sits on the Iron Throne; Daenerys sails across the Narrow Sea; Jon Snow is King in the North, and winter is finally here.", mediumImageID: "314502", originalImageID: "314502")
     ]
+    
+    func seasonSections() -> [[GOTEpisode]] {
+        
+        let sortedSeasons = GOTEpisode.allEpisodes.sorted { $0.season < $1.season }
+        
+        let completeSeasons = Set(sortedSeasons.map {$0.season})
+        
+        var sections = Array(repeating: [GOTEpisode](), count: completeSeasons.count)
+        
+        var currentIndex = 0
+        var currentSeason = sortedSeasons.first?.season ?? 0
+        
+        for seasonNum in sortedSeasons {
+            if seasonNum.season == currentSeason {
+                sections[currentIndex].append(seasonNum)
+            } else {
+                currentIndex += 1
+                sections[currentIndex].append(seasonNum)
+                currentSeason = seasonNum.season
+            }
+        }
+        
+        return sections
+}
 }
